8c672aa12c0caec58eb9a84c4e35a0d688bafde1
volpinottv <108951418+volpinottv@users.noreply.github.com>
2022-12-20 22:00:14 +0100

    v1.1

--- README.md
+++ README.md
@@ -1,26 +1,22 @@
-# Auto VPN V 1.0 (italian tool)
+# Auto TOR V 1.1
 
-how to install this tools :
+How to install this tools
 
-* : requirements:
+1 : git clone https://github.com/volpinottv/AutoTOR
 
-  sudo apt-get install tor
-  pip3 install requests[socks]
-  or just run autoTor it will install everything
-
-1 : git clone 
-
-2 : cd AutoVPN
+2 : cd AutoTOR
 
 3 : python3 install.py
 
-4 : interminal type ( aut ) any where you want
+4 : in terminal write -> aut
   
 5 : type time to change IP
 
-6 : type how many time to change your ip 
+6 : type how many time to change your ip
+![Immagine 2022-12-18 135922](https://user-images.githubusercontent.com/108951418/208300435-49b0cd2f-b8e6-47bb-9a3e-c8cde8c531df.png)
 
 7 : go to your browser / pc  change sock proxy to 127.0.0.1:9050
+![Immagine 2022-12-18 141903](https://user-images.githubusercontent.com/108951418/208300608-416aeb9b-657d-425f-a27c-f40cce7b1f4b.png)
 
-8 : DONE!
-
+8 : your pc is completely anonymous!
+![Immagine 2022-12-18 140107](https://user-images.githubusercontent.com/108951418/208300704-63e2c878-6142-43a5-b7a7-1d73942d7160.png)
--- install.py
+++ install.py
@@ -1,18 +1,22 @@
 import os
-choice = input('\033[0;31mPer installarlo premi (Y) Altrimenti premi (N) -> ')
+import time
+choice = input('\033[1;36m\nPer installare [aut] premi (Y) Per rimuoverlo premi (N) -> ')
 run = os.system
 if str(choice) =='Y' or str(choice)=='y':
 
-    run('chmod 777 AutoVPN.py')
+    run('chmod 777 AutoTOR.py')
     run('mkdir /usr/share/aut')
-    run('cp AutoVPN.py /usr/share/aut/AutoVPN.py')
+    run('cp AutoTOR.py /usr/share/aut/AutoTOR.py')
 
-    cmnd=(' #! /bin/sh \n exec python3 /usr/share/aut/AutoVPN.py "$@"')
+    cmnd=(' #! /bin/sh \n exec python3 /usr/share/aut/AutoTOR.py "$@"')
     with open('/usr/bin/aut','w')as file:
         file.write(cmnd)
-    run('chmod +x /usr/bin/aut & chmod +x /usr/share/aut/AutoVPN.py')
-    print('''\n\nAutoVPN INSTALLATA CORRETTAMENTE! \nfrom now just type \x1b[6;30;42maut\x1b[0m in terminal ''')
+    run('chmod +x /usr/bin/aut & chmod +x /usr/share/aut/AutoTOR.py')
+    print("\033[1;37m\nAutoTOR INSTALLATA CORRETTAMENTE!")
+    time.sleep(1)
+    print("\033[1;36m\nPer aprire il programma digita: aut")
 if str(choice)=='N' or str(choice)=='n':
     run('rm -r /usr/share/aut ')
     run('rm /usr/bin/aut ')
-    print('\033[0;31mProcesso annullato')
+    print("\033[1;37m\nComando [aut] rimosso")
+    
